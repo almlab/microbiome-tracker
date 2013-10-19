128edb467c3acbc9152a0fd0d1f335110e397e6b
Albert Wang <albertyw@mit.edu>
2013-10-19 19:13:40 -0400

    Added simple annotate page

--- src/serve.py
+++ src/serve.py
@@ -15,14 +15,17 @@ def new_file_name(i = 0):
     else:
         return location
 
+# Default page that allows user to take pictures and upload them
 @app.route("/")
 def root():
     return render_template('index.htm')
 
+# Utility page to load static files
 @app.route('/static/<path:filename>')
 def static_files(filename):
     return send_from_directory('static/', filename)
 
+# Page for server to receive photos
 @app.route('/receive_photo/', methods=['POST'])
 def receive_photo():
     f = open(new_file_name(), 'wb')
@@ -30,6 +33,17 @@ def receive_photo():
     cleaned_data = raw_data[raw_data.find(',')+1:]
     f.write(base64.decodestring(cleaned_data))
     f.close()
+    return ''
+
+# Page for users to annotate photos
+@app.route('/annotate/')
+def annotate_photo():
+    img_src = '/static/photos/1.jpg'
+    return render_template('annotate.htm', img_src=img_src)
+
+# Page for users to see their history
+@app.route('/history/')
+def history():
     return ''
 
 
